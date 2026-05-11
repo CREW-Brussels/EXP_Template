95463d31560ce4a5ebbeba77e334e2cd0251d247
haryo <haryo@crew.brussels>
2026-05-11 13:56:51 +0200

    First update to UE5.7

--- Source/EXP_TemplateEditor.Target.cs
+++ Source/EXP_TemplateEditor.Target.cs
@@ -8,8 +8,8 @@ public class EXP_TemplateEditorTarget : TargetRules
 	public EXP_TemplateEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("EXP_Template");
 	}
 }
